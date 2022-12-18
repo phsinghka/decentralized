df9e501cb6652e8b9675e0fe7e7b9460df71d7c0
Piyush Himmatsinghka <phsinghka@Piyushs-Air.ht.home>
2022-12-17 23:51:43 -0500

    Contract Changes

--- smart_contract/contracts/Insurance.sol
+++ smart_contract/contracts/Insurance.sol
@@ -22,6 +22,8 @@ contract Insurance {
         uint coverageAmount;
         bool isInsuranceActive;
         bool isClaimActive;
+        uint nextInstallment;
+        uint startDate;
         uint claimAmount;
         address claimHospitalAddress;
     }
@@ -54,14 +56,27 @@ contract Insurance {
         _;
     }
 
+    modifier InsuranceActive(address addr){
+        require(clientMap[addr].isInsuranceActive,'Insrance on this address is not active');
+        _;
+    }
+
     //GET FUNCTIONS
 
-    function getInsuredAmount(address addr) public view isHospital returns(uint){
+    function getInsuredAmount(address addr) public view isHospital InsuranceActive(addr) returns(uint){
         return clientMap[addr].coverageAmount;
     }
 
-    function getInsuranceStatus(address addr) public view isHospital returns(bool){
+    function getInsuranceStatus(address addr) public view isHospital InsuranceActive(addr) returns(bool){
         return clientMap[addr].isInsuranceActive;
+    }
+
+    function getInsuranceStartDate(address addr) public view InsuranceActive(addr) returns(uint) {
+        return clientMap[addr].startDate;
+    }
+
+    function getInsuranceNextInstallment(address addr) public view InsuranceActive(addr) returns(uint) {
+        return clientMap[addr].nextInstallment;
     }
 
     
@@ -81,25 +96,36 @@ contract Insurance {
         clientMap[msg.sender].coverageAmount =  10 ether;
         clientMap[msg.sender].isClaimActive =  false;
         clientMap[msg.sender].isInsuranceActive =  true;
+        clientMap[msg.sender].nextInstallment = block.timestamp + 30 days;
+        clientMap[msg.sender].startDate = block.timestamp;
     }
 
-    function payPremium() public payable{
+    function payPremium(address addr) public payable{
         require(msg.value == 0.01 ether,'Premium should be in 0.01 Ether');
-        uint temp = clientMap[msg.sender].monthlyPremium + msg.value;
-        clientMap[msg.sender].monthlyPremium =  temp;
+        uint temp = clientMap[addr].monthlyPremium + msg.value;
+        clientMap[addr].monthlyPremium =  temp;
+        clientMap[addr].isInsuranceActive = true;
+        clientMap[addr].nextInstallment = clientMap[addr].nextInstallment + 30 days;
     }
 
-    function acceptClaim() public isClient{
+    function acceptClaim() public payable isClient{
+        require(clientMap[msg.sender].isClaimActive,'Client has no claim active');
         require(address(this).balance>= clientMap[msg.sender].claimAmount, 'Not Enough balance in Contract. Please Try again !!');
         uint temp = clientMap[msg.sender].coverageAmount - clientMap[msg.sender].claimAmount;
         clientMap[msg.sender].coverageAmount = temp;
-        payable(address(this)).transfer(clientMap[msg.sender].claimAmount);
+        temp = clientMap[msg.sender].claimAmount;
+        clientMap[msg.sender].claimAmount = 0;
+        payable(clientMap[msg.sender].claimHospitalAddress).transfer(temp);
+        clientMap[msg.sender].isClaimActive = false;
     }
 
-    function fileClaim(address clientAddress, uint claimAmount) public isHospital{
-        require(!clientMap[clientAddress].isInsuranceActive,'Client Insurance is not active, Please ak them to pay the premium !');
+    function fileClaim(address clientAddress, uint claimAmount) public isHospital InsuranceActive(clientAddress){
+        if(block.timestamp>clientMap[clientAddress].nextInstallment){
+            clientMap[clientAddress].isInsuranceActive = false;
+        }
+        claimAmount = claimAmount * (10 ** 18);
         require(!clientMap[clientAddress].isClaimActive,'Client Has another active claim, Please resolve the previos claim to file new claim !');
-        require(address(this).balance>= claimAmount, 'Not Enough balance in Contract. Please Try again !!');
+        require(address(this).balance>= claimAmount , 'Not Enough balance in Contract. Please Try again !!');
         require(clientMap[clientAddress].coverageAmount >= claimAmount, 'Claim is bigger than remaining coverage amount');
 
         clientMap[clientAddress].claimAmount = claimAmount;
